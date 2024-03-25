2557ea9bb13ca73bb8c6f5e5146b9a40b54111df
yechop <channetattoo@gmail.com>
2024-03-25 22:16:13 +0900

    test: 체스보드의 기물 움직임 테스트 추가

--- src/test/java/domain/board/ChessBoardTest.java
+++ src/test/java/domain/board/ChessBoardTest.java
@@ -4,12 +4,14 @@ import fixture.PieceFixture;
 import fixture.PositionFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ChessBoardTest {
 
@@ -93,5 +95,28 @@ public class ChessBoardTest {
                 Map.entry(PositionFixture.g2(), PieceFixture.whitePawn()),
                 Map.entry(PositionFixture.h2(), PieceFixture.whitePawn())
         );
+    }
+
+    @DisplayName("source 에서 target 으로 기물을 옮긴다.")
+    @Nested
+    class MovePiece {
+
+        @BeforeEach
+        void setUp() {
+            chessBoard.move(PositionFixture.a2(), PositionFixture.a4());
+        }
+
+        @DisplayName("움직인 후 기존 위치의 기물은 제거된다.")
+        @Test
+        void removePieceAfterMove() {
+            assertThatThrownBy(() -> chessBoard.findPiece(PositionFixture.a2()))
+                    .isInstanceOf(IllegalArgumentException.class).hasMessage("해당 위치에 기물이 없습니다.");
+        }
+
+        @DisplayName("목적지에 기물을 놓는다.")
+        @Test
+        void putPieceAfterMove() {
+            assertThat(chessBoard.findPiece(PositionFixture.a4())).isEqualTo(PieceFixture.whitePawn());
+        }
     }
 }
